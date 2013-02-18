a90af309e770a0e02e2d7957969e31d6fc4b0d0b
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-18 16:47:51 -0500

    Created the find location component html
    Created the barebones of the search component. No styling at the
    moment. Just created it so we can start to work on linking it together
    and get some backend working

---
+++ application/views/search-view.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<title>untitled</title>
+</head>
+<body>
+	<section class="findLocation">
+		<section class="findLocation-Close">
+			<button class="btn btn-large btn-block btn-danger" type="button">Close</button>
+
+		</section><!-- end of findLocation Close-->
+		<section class="searchkeyword">
+			<form class="form-search">
+				<section class="input-append">
+					<input type="text" class="span2 search-query">
+						<button type="submit" class="btn">Search</button>
+				</section>
+		</section><!-- end of searchkeyword-->
+		
+		<section class="dropdownBuildings">
+			<select multiple="multiple">
+				<option>FS1</option>
+				<option>FS2</option>
+				<option>FS3</option>
+				<option>FS4</option>
+			</select>
+		</section><!-- end fo dropdownBuildings-->
+		
+		<section class="poiSelection">
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox1" value="option1"> Patio
+		</label>
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox2" value="option2"> Food/Drink
+		</label>
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox3" value="option3"> Library
+		</label>
+		
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox3" value="option3"> Book Store
+		</label>
+		
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox3" value="option3"> Restroom
+		</label>
+		<label class="checkbox inline">
+			<input type="checkbox" id="inlineCheckbox3" value="option3"> Receptionist
+		</label>
+		</section><!-- end o poi selection-->
+		
+		<section class="findlocationBtns">
+		
+			<section class="suggestSpotbtn">
+				<a href="#" class="btn btn-large btn-block btn-primary disabled">Suggest a Spot</a>
+
+			</section>!-- end of suggest spot btn-->
+			
+			<section class="viewLocationbtn">
+			<button class="btn btn-large btn-block btn-success" type="button">Block level button</button>
+
+			</section><!-- end of view location btn-->
+		
+		</section><!-- end of find location btns-->
+	</section><!-- end of findlocation-->
+</body>
+</html>	
