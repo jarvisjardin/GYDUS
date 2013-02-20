36498c514e0b461e7fa95ed307e9248ce089d992
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-20 18:12:39 -0500

    Created Account Settings Html and working on css

--- application/views/mapView.php
+++ application/views/mapView.php
@@ -77,7 +77,7 @@
 		<section class="findlocationBtns">
 		
 			<section class="suggestSpotbtn">
-				<a href="#" class="btn btn-large btn-block  disabled">Suggest a Spot</a>
+				<a href="#" class="btn btn-large btn-block  ">Suggest a Spot</a>
 
 			</section><!-- end of suggest spot btn-->
 			<br>
@@ -91,8 +91,28 @@
 		
 		
 		
+		<!---------------------------------------------------
 		
 		
+		
+		 Account Settings Menu
+		 
+		 
+		 
+		 ---------------------------------------------------->
+		
+		<!-- this is going to have a class of hide -->
+		
+		<section class="acctMenu">
+			<section class="acctSettings alert alert-info">
+				<h3 class="accountSettingsHead">Account Settings</h3>
+			</section><!-- end of acctSettings-->
+			
+			<section class="logOutacctSettings alert alert-error">
+				<h3 class="logOutaccountSettingsHead">Sign Out</h3>
+			</section><!-- end of logoutacctSettings-->
+		
+		</section><!--- end of acctMenu--->
 		
 		
 		
@@ -112,7 +132,8 @@
 		
 		
 
-		<h4><?php echo anchor('index.php/userController/logout', 'Logout'); ?></h4>
+<!-- 		<h4><?php echo anchor('index.php/userController/logout', 'Account Settings'); ?></h4> -->
+		<h4>Account Settings link</h4>
 		<h4 class="map-view-fL btn btn-large btn-block btn-info" id="map-viewFLbtn">Find Location</h4>
 <!-- 		<?php echo anchor('index.php/mapController/searchView', 'Find Location'); ?> -->
 <!-- 		<h4><?php echo anchor('index.php/mapController/suggestaspotView', 'Suggest A Spot'); ?></h4> -->
