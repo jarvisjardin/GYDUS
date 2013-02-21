8e796dfc838c7ba9feedbc1124498196b25d51ec
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-20 19:42:17 -0500

    Removed the sections for errors on the login form

--- application/views/loginView.php
+++ application/views/loginView.php
@@ -9,20 +9,15 @@
             <section class="control-group gydus-form-error">
               <label class="control-label" for="inputEmail">Email</label>
               <section class="controls">
-              	<section class="loginEmailError">
                 <input type="text" class="gydus-login-email-input" name="user_email" id="login-inputEmail" placeholder="Email">
-                   <p class="loginemailerr">invalid email</p>
-
-              	</section><!--end of loginEmailError-->
+                 
               </section><!-- end of controls -->
             </section><!-- end of email-->
             <section class="control-group">
               <label class="control-label" for="inputPassword">Password</label>
               <section class="controls">
-                 <section class="loginPwError">
                 <input type="password" id="login-inputPassword" name="user_pass" placeholder="Password">
-                <p class="loginpasserr">invalid password</p>
-                 </section><!-- end of loginPwError -->
+               
               </section><!-- end of controls-->
             </section><!-- end of password-->
             <section class="control-group">
