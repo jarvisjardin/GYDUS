3ab980600adfc43f046be0da2aebd847f591536c
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-21 20:31:27 -0500

    Added class to successemail for styling

--- application/views/emailSentView.php
+++ application/views/emailSentView.php
@@ -1,4 +1,4 @@
-<section>
+<section class="gydusSuccessEmail">
 
 <h1>Thank you for contacting us, We will get back to you as soon as possible.</h1>
 <button class="btn btn-large btn-primary" type="button" onClick="location='<?php echo base_url();?>index.php/mapController/'">Back to Gydus</button>
