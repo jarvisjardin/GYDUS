e3955aec00297a33cb55b35f65f62e9a3992a1f9
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-21 22:36:26 -0500

    Added Styled Footer (icons only)

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,15 +1,35 @@
 	
 	</section>
 	</section>
-	<section id='footer' class="gydus-footernav navbar">
-		<section class="foot-container navbar-inner">
-			<ul class="footerUl nav" id="footerULID">
+	<!--
+<section id='footer' class="gydus-footernav">
+		<section class="foot-container">
+			<ul class="footerUl">
 				<li class="footNavBtn" id="FootNavBtn"><a class="footNavBtna" href="#">NavMe</a></li>
 				<li class="footGPSBtn" id="FootGPSBtn"><a class="footGPSBtna" href="#">GPS</a></li>
 				<li class="footSAS" id="FootSASBtn"><a class="footSASa" href="#">SAS</a></li>
 			</ul><!-- end of ul-->
 		</section><!-- end of foot-container-->
 	</section><!-- end of footer-->
+
+
+
+
+
+<section class="footer row pull-left">
+	<section class="foot-nav span5 pull-left clearfix">
+		
+	</section>
+	
+	<section class="foot-GPS span5 pull-left clearfix">
+	
+	</section>
+	
+	<section class="foot-sas span5 pull-left clearfix">
+	
+	</section>
+	
+</section><!-- end of footer -->
 </body>
 <!-- jquery -->
 
