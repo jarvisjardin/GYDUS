8a2cafbdb76ac2d37184e78f81e50ad2bff6a32e
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-02-26 19:42:26 -0500

    removed bg on map. (this commit is just so jarvis can get my footer)

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,16 +1,7 @@
 	
 	</section>
 	</section>
-	<!--
-<section id='footer' class="gydus-footernav">
-		<section class="foot-container">
-			<ul class="footerUl">
-				<li class="footNavBtn" id="FootNavBtn"><a class="footNavBtna" href="#">NavMe</a></li>
-				<li class="footGPSBtn" id="FootGPSBtn"><a class="footGPSBtna" href="#">GPS</a></li>
-				<li class="footSAS" id="FootSASBtn"><a class="footSASa" href="#">SAS</a></li>
-			</ul><!-- end of ul-->
-		</section><!-- end of foot-container-->
-	</section><!-- end of footer-->
+
 
 <section id="footer" class="row">
 	 <button class="foot-nav" type="button"></button>
@@ -22,19 +13,6 @@
 
 
 
-<!--
-<section class="footer row pull-left">
-	<section class="foot-nav span5 pull-left clearfix">
-		
-	</section>
-	
-	<section  id="#footGPS" class="foot-GPS span5 pull-left clearfix">
-		<p id="clickableGPS">GPS</p>	</section>
-	
-	<section class="foot-sas span5 pull-left clearfix">
-	
-	</section>
--->
 	
 </section><!-- end of footer -->
 </body>
