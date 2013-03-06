24e27c63759d74ffc5f8692dca9b315f89c59bdd
Geoffrey Ganga <bakerskategg09@gmail.com>
2013-03-05 19:08:23 -0500

    styled the contact us page

--- application/views/contactUsView.php
+++ application/views/contactUsView.php
@@ -1,11 +1,17 @@
-<section class="gydus-register">
+<section class="gydus-terms-titlebtn">
+				<h3 class="btn btn-large btn-block btn-info"><a href="<?php echo base_url();?>index.php/mapController/">Contact Us</a></h3>
+				<section class="terms-back">
+					<button class="gydus-decorner-btn btn  btn-link pull-right clearfix" id="findLbtnX" type="button" ><</button>
+
+				</section><!-- end of findLocation Close-->
+<section class="gydus-contactus">
 <h1 class="gydus-register-headertxt">Contact Us</h1>
 
 <form action="<?php echo base_url();?>index.php/userController/send_email"  method="post" class="form-horizontal gydus-form-horizontal ">
 
 
 		<!-- NAME OF SPOT-->
-		<section class="control-group">
+		<section class="gyduscontact control-group">
               <label class="control-label" for="inputName">Name of Spot</label>
               <section class="controls">
                 <input type="text" class="gydus-reg-name-input" name="reginputName"  id="reg-inputName reginputName" placeholder="Name of Spot">
@@ -13,7 +19,7 @@
               
              <!-- EMAIL-->
 
-            <section class="control-group">
+            <section class="gyduscontact control-group">
               <label class="control-label" for="inputEmail">How do we reach you?</label>
               <section class="controls">
                 <input type="text" class="gydus-reg-email-input" name="reginputEmail" id="reg-inputEmail reginputEmail" placeholder="Email you can be reached at">
@@ -33,7 +39,7 @@
                 </section>
                 <!-- end fo dropdownBuildings-->
 
-		<section class="control-group">
+		<section class="gyduscontact control-group">
               <section class="controls">
 	              <textarea rows="3" id="gydusContactMes" name="gydusContactMes" placeholder="Drop us a messege"></textarea>             
 	               </section>
@@ -43,10 +49,9 @@
             <section class="control-group">
               <section class="controls">
                 
-                <button class="btn btn-success" type="submit" class="btn">SIGN UP</button>
+                <button class="btn btn-success" type="submit" class="btn">SUBMIT</button>
               </section>
             </section>
           </form>
-<button class="btn btn-block btn-warning" type="button"  onClick="location='<?php echo base_url();?>index.php/mapController/'">Go back Home</button>
 
           </section><!-- end of Gydus Login-->
