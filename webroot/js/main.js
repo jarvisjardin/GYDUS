4d2bac02f3ba1321fdd2062a978f6a332ec70f38
Jarvis Jardin <jjardin@fullsail.edu>
2013-03-05 19:38:43 -0500

    fixed the centermap error
    - the gydus button didn't, center the map like before.
    - A Variable was removed that was references in the click event

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -9,283 +9,258 @@
       				  (201) 779-6182
       				  
    ------------------------------------------
-	The below code is accessing the Google Maps APIv3 and the Google Places API
-	Also using maxmind for ip location
+
 
 */
-$(document).ready(function() {
-	var snd = document.createElement('audio'),
-			    src = document.createElement('source'); 
-			    
-			    src.src = "http://s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3"; 
-			    src.type = "audio/mp3"; 
-			    src.loop = 'true';
-			    snd.appendChild(src); 
-			    snd.preload = 'null'; 
+$(document).ready(function () {
+    
+    var snd = document.createElement('audio'),
+        src = document.createElement('source');
+   
+    var FullSail = new google.maps.LatLng(28.594461, -81.304002);
 
-			  			   
-		
-	var success = function() {
-			console.log('Konami Success');			
-			alert("DO THE HARLEMSHAKE");
-			
-			
-			   
-			   //$('#gydusHeader').animate({margin-right: 1em});
-			
-			
-			 harlemShake()
-			 setTimeout(function(){snd.play()},900);
 
-			 setTimeout(function(){history.go(0)},32000)							  
-	
-			  
-			   // here you can use anything you defined in the loaded script
-			   
+    src.src = "http://s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";
+    src.type = "audio/mp3";
+    src.loop = 'true';
+    snd.appendChild(src);
+    snd.preload = 'null';
 
-	}
-	
-	function stoper()	{
-			clearTimeout(timer);
-	}
-		
-	var konami = new Konami(success);
 
-/*------REGISTER FRONT SIDE VALIDATION---------*/
+
+    var success = function () {
+        console.log('Konami Success');
+        alert("DO THE HARLEMSHAKE");
+
+        harlemShake()
+        setTimeout(function () {
+            snd.play()
+        }, 900);
+
+        setTimeout(function () {
+            history.go(0)
+        }, 32000)
+    };
+    
+    var konami = new Konami(success);
+
+    /*------REGISTER FRONT SIDE VALIDATION---------*/
 
 
 
 
-  /*--       REGISTER USERNAME VALIDATION      --*/
+    /*--       REGISTER USERNAME VALIDATION      --*/
 
-$('.gydus-reg-name-input').keydown(function(event){
-	 if($('.gydus-reg-name-input').val().length < 4){
-		 $('.gydus-reg-name-input').css("border", "1px solid red");
-		 		 		 $('.wronguser').removeClass('hide');
+    $('.gydus-reg-name-input').keydown(function (event) {
+        if ($('.gydus-reg-name-input').val().length < 4) {
+            $('.gydus-reg-name-input').css("border", "1px solid red");
+            $('.wronguser').removeClass('hide');
 
-	 }else{
-		 		 $('.gydus-reg-name-input').css("border", "2px solid green");
-		 		 $('.checkuser').removeClass('hide');
-		 		 $('.wronguser').addClass('hide');
+        } else {
+            $('.gydus-reg-name-input').css("border", "2px solid green");
+            $('.checkuser').removeClass('hide');
+            $('.wronguser').addClass('hide');
 
-	 }
- })
- 
- 
-  /*--       REGISTER PASSWORD VALIDATION      --*/
-
- $('#reg-inputPassword').keydown(function(event){
-	 if($('#reg-inputPassword').val().length < 5){
-		 $('#reg-inputPassword').css("border", "1px solid red");
-		 $('.wrongpass').removeClass('hide');
-
-	 }else{
-		 		 $('#reg-inputPassword').css("border", "2px solid green");
-		 		 $('.checkpass').removeClass('hide');
-		 		 $('.wrongpass').addClass('hide');
-	 }
- })
- 
- 
- 
- /*--       REGISTER EMAIL VALIDATION      --*/
-  $('.gydus-reg-email-input').keydown(function(event){
-  
-  	var regEmailInput = $('.gydus-reg-email-input').val()
-  	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-
-	 if(regEmailInput==''){
-		 $('.gydus-reg-email-input').css("border", "1px solid red");
-		 		 $('.wrongemail').removeClass('hide');
+        }
+    })
 
 
-	 }else if(!emailReg.test(regEmailInput)){
-		 $('.gydus-reg-email-input').css("border", "1px solid red");
-		 $('.wrongemail').removeClass('hide');
+    /*--       REGISTER PASSWORD VALIDATION      --*/
 
-	 }else{
-		 $('.gydus-reg-email-input').css("border", "2px solid green");
-		 $('.checkemail').removeClass('hide');
-		 $('.wrongemail').addClass('hide');
+    $('#reg-inputPassword').keydown(function (event) {
+        if ($('#reg-inputPassword').val().length < 5) {
+            $('#reg-inputPassword').css("border", "1px solid red");
+            $('.wrongpass').removeClass('hide');
 
-
-	 }
- })
+        } else {
+            $('#reg-inputPassword').css("border", "2px solid green");
+            $('.checkpass').removeClass('hide');
+            $('.wrongpass').addClass('hide');
+        }
+    })
 
 
 
-/*------LOGIN FRONT SIDE VALIDATION---------*/
+    /*--       REGISTER EMAIL VALIDATION      --*/
+    $('.gydus-reg-email-input').keydown(function (event) {
 
- /*--       LOGIN EMAIL VALIDATION      --*/
-  $('.gydus-login-email-input').keydown(function(event){
-  
-  	var regEmailInput = $('.gydus-login-email-input').val()
-  	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        var regEmailInput = $('.gydus-reg-email-input').val()
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-	 if(regEmailInput==''){
-		 $('.gydus-login-email-input').css("border", "1px solid red");
-		 		 $('.wrongemail').removeClass('hide');
+        if (regEmailInput == '') {
+            $('.gydus-reg-email-input').css("border", "1px solid red");
+            $('.wrongemail').removeClass('hide');
 
 
-	 }else if(!emailReg.test(regEmailInput)){
-		 $('.gydus-login-email-input').css("border", "1px solid red");
-		 $('.wrongemail').removeClass('hide');
+        } else if (!emailReg.test(regEmailInput)) {
+            $('.gydus-reg-email-input').css("border", "1px solid red");
+            $('.wrongemail').removeClass('hide');
 
-	 }else{
-		 $('.gydus-login-email-input').css("border", "2px solid green");
-		 $('.checkemail').removeClass('hide');
-		 $('.wrongemail').addClass('hide');
+        } else {
+            $('.gydus-reg-email-input').css("border", "2px solid green");
+            $('.checkemail').removeClass('hide');
+            $('.wrongemail').addClass('hide');
 
 
-	 }
- })
- 
- 
-   /*--       LOGIN PASSWORD VALIDATION      --*/
-
- $('#login-inputPassword').keydown(function(event){
-	 if($('#login-inputPassword').val().length < 5){
-		 $('#reg-inputPassword').css("border", "1px solid red");
-		 $('.wrongpass').removeClass('hide');
-
-	 }else{
-		 		 $('#login-inputPassword').css("border", "2px solid green");
-		 		 $('.checkpass').removeClass('hide');
-		 		 $('.wrongpass').addClass('hide');
-	 }
- })
- 
- 
- 
- /*------SUGGEST A SPOT FRONT SIDE VALIDATION---------*/
+        }
+    })
 
 
 
+    /*------LOGIN FRONT SIDE VALIDATION---------*/
 
-  /*--       SAS NAME VALIDATION      --*/
+    /*--       LOGIN EMAIL VALIDATION      --*/
+    $('.gydus-login-email-input').keydown(function (event) {
 
-$('#sasSpotname').keydown(function(event){
-	 if($('#sasSpotname').val().length < 2){
-		 $('#sasSpotname').css("border", "1px solid red");
-		 		 		 $('.wrongsasSpotname').removeClass('hide');
+        var regEmailInput = $('.gydus-login-email-input').val()
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-	 }else{
-		 		 $('#sasSpotname').css("border", "2px solid green");
-		 		 $('.checksasSpotname').removeClass('hide');
-		 		 $('.wrongsasSpotname').addClass('hide');
+        if (regEmailInput == '') {
+            $('.gydus-login-email-input').css("border", "1px solid red");
+            $('.wrongemail').removeClass('hide');
 
-	 }
- })
- 
- 
-  /*--       SAS DESC VALIDATION      --*/
 
- $('.gydus-kw-search-spotDesc').keydown(function(event){
-	 if($('.gydus-kw-search-spotDesc').val().length < 2){
-		 $('.gydus-kw-search-spotDesc').css("border", "1px solid red");
-		 $('.wrongsasdesc').removeClass('hide');
+        } else if (!emailReg.test(regEmailInput)) {
+            $('.gydus-login-email-input').css("border", "1px solid red");
+            $('.wrongemail').removeClass('hide');
 
-	 }else{
-		 		 $('.gydus-kw-search-spotDesc').css("border", "2px solid green");
-		 		 $('.checksasdesc').removeClass('hide');
-		 		 $('.wrongsasdesc').addClass('hide');
-	 }
- })
- 
+        } else {
+            $('.gydus-login-email-input').css("border", "2px solid green");
+            $('.checkemail').removeClass('hide');
+            $('.wrongemail').addClass('hide');
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+        }
+    })
+
+
+    /*--       LOGIN PASSWORD VALIDATION      --*/
+
+    $('#login-inputPassword').keydown(function (event) {
+        if ($('#login-inputPassword').val().length < 5) {
+            $('#reg-inputPassword').css("border", "1px solid red");
+            $('.wrongpass').removeClass('hide');
+
+        } else {
+            $('#login-inputPassword').css("border", "2px solid green");
+            $('.checkpass').removeClass('hide');
+            $('.wrongpass').addClass('hide');
+        }
+    })
+
+
+
+    /*------SUGGEST A SPOT FRONT SIDE VALIDATION---------*/
 
 
 
 
+    /*--       SAS NAME VALIDATION      --*/
 
-   // variable to get fullsails latitude and long
+    $('#sasSpotname').keydown(function (event) {
+        if ($('#sasSpotname').val().length < 2) {
+            $('#sasSpotname').css("border", "1px solid red");
+            $('.wrongsasSpotname').removeClass('hide');
 
-        /*-----------Find location click effects---------------*/
-         $('#map-viewFLbtn').on('click', function(){   
-           $('#map-viewFLbtn').fadeOut(1500, function(){
-					$('#map-viewFLbtn').addClass('hide');
-					$('#fldropcarret').addClass('hide');
-					$('#mapSpot').addClass('hide');	
-					$('#footer').addClass('hide');
-				});	
-			
-			$("#searchViewdisplay").slideDown(1500,function(){
-				$("input[type=text]").val('');
-				
-				
-			});
-			
+        } else {
+            $('#sasSpotname').css("border", "2px solid green");
+            $('.checksasSpotname').removeClass('hide');
+            $('.wrongsasSpotname').addClass('hide');
+
+        }
+    })
 
 
-	         return false;
-	         
-	     });// END OF displaying find location   
-	     
-	     
-	     
-	     	$("#findLbtnX").on('click', function(){
-	     		$("#searchViewdisplay").slideUp(1500);
-				$('#map-viewFLbtn').fadeIn(1000, function(){
-					$('#map-viewFLbtn').removeClass('hide');
-					$('#fldropcarret').removeClass('hide');
+    /*--       SAS DESC VALIDATION      --*/
 
-				});					$('#mapSpot').removeClass('hide');	$('#footer').removeClass('hide');	
+    $('.gydus-kw-search-spotDesc').keydown(function (event) {
+        if ($('.gydus-kw-search-spotDesc').val().length < 2) {
+            $('.gydus-kw-search-spotDesc').css("border", "1px solid red");
+            $('.wrongsasdesc').removeClass('hide');
 
-		     	
-	     	})// END of Closing the Finding Location
+        } else {
+            $('.gydus-kw-search-spotDesc').css("border", "2px solid green");
+            $('.checksasdesc').removeClass('hide');
+            $('.wrongsasdesc').addClass('hide');
+        }
+    })
 
 
-/*-----------View Acct Menu---------------*/
+    // variable to get fullsails latitude and long
 
-	
-	$("#viewAcctMenu").on('click', function(){
-		
-		$("#accountMenu").removeClass("hide");
-		$("#viewAcctMenu").addClass("hide");
-	});
-	
-	$("#CloseMenu").on('click', function(){
-		
-		$("#accountMenu").addClass("hide");
-		$("#viewAcctMenu").removeClass("hide");
+    /*-----------Find location click effects---------------*/
+    $('#map-viewFLbtn').on('click', function () {
+        $('#map-viewFLbtn').fadeOut(1500, function () {
+            $('#map-viewFLbtn').addClass('hide');
+            $('#fldropcarret').addClass('hide');
+            $('#mapSpot').addClass('hide');
+            $('#footer').addClass('hide');
+        });
 
-		
-	});
-	
-	
-	//user clicks Accounts Settings from the menu
-	$("#AccountSettingsSec").on('click', function(){
-		
-		
-	});
-	
-	
-	
-	$("#updateInfobtn").on('click', function(){
-		$('#accountsuccess').removeClass('hide');
-		
-	})
-	
-	
-	/*-----------View Gydus Menu---------------*/
+        $("#searchViewdisplay").slideDown(1500, function () {
+            $("input[type=text]").val('');
 
-	
-/*
+
+        });
+
+
+
+        return false;
+
+    }); // END OF displaying find location   
+
+
+
+    $("#findLbtnX").on('click', function () {
+        $("#searchViewdisplay").slideUp(1500);
+        $('#map-viewFLbtn').fadeIn(1000, function () {
+            $('#map-viewFLbtn').removeClass('hide');
+            $('#fldropcarret').removeClass('hide');
+
+        });
+        $('#mapSpot').removeClass('hide');
+        $('#footer').removeClass('hide');
+
+
+    }) // END of Closing the Finding Location
+
+
+    /*-----------View Acct Menu---------------*/
+
+
+    $("#viewAcctMenu").on('click', function () {
+
+        $("#accountMenu").removeClass("hide");
+        $("#viewAcctMenu").addClass("hide");
+    });
+
+    $("#CloseMenu").on('click', function () {
+
+        $("#accountMenu").addClass("hide");
+        $("#viewAcctMenu").removeClass("hide");
+
+
+    });
+
+
+    //user clicks Accounts Settings from the menu
+    $("#AccountSettingsSec").on('click', function () {
+
+
+    });
+
+
+
+    $("#updateInfobtn").on('click', function () {
+        $('#accountsuccess').removeClass('hide');
+
+    })
+
+
+    /*-----------View Gydus Menu---------------*/
+
+
+    /*
 	$("#logo").on('click', function(){
 		
 		$("#gydusMenu").removeClass("hide");
@@ -299,47 +274,44 @@ $('#sasSpotname').keydown(function(event){
 	});
 	
 */
-	
 
-	
-	
 
-/*---------- GPS Position -----------------*/
 
-$('#gpsBtn').click(function (e) { //enables the use of GPS and moved the user's marker to there location 
-		
-		//console.log("hello");
-		map.panTo(FullSail);
+
+
+    /*---------- GPS Position -----------------*/
+
+    $('#gpsBtn').click(function (e) { //enables the use of GPS and moved the user's marker to there location 
+
+        //console.log("hello");
+        map.panTo(FullSail);
 
         return false;
     });
 
-	
-	
-	
-	$("#sasBtnsubmit").on('click', function(){
-		
-		$("#sasSuccess").removeClass("hide");
-	
-	});
+    $("#sasBtnsubmit").on('click', function () {
 
-	        
-	        
-	        
-/*---------- flashdata time out -----------------*/
-$('#sasPromptsuccess').fadeOut(3000, function(){
-	$('#sasPromptsuccess').addClass('hide');
-});
-    
-/*---------- flashdata time out -----------------*/
-$('#editAccPromptsuccess').fadeOut(3000, function(){
-	$('#editAccPromptsuccess').addClass('hide');
-});
-    
-    
-    
-    
-    
-    
-                         
+        $("#sasSuccess").removeClass("hide");
+
+    });
+
+
+
+
+    /*---------- flashdata time out -----------------*/
+    $('#sasPromptsuccess').fadeOut(3000, function () {
+        $('#sasPromptsuccess').addClass('hide');
+    });
+
+    /*---------- flashdata time out -----------------*/
+    $('#editAccPromptsuccess').fadeOut(3000, function () {
+        $('#editAccPromptsuccess').addClass('hide');
+    });
+
+
+
+
+
+
+
 });
